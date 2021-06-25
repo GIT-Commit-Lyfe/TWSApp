8e99bc0aedc312d199edfe6b67b9dd63fcac35f6
lutfi <mlutfiibra@outlook.com>
2021-06-25 21:53:34 +0700

    Finishing accounts menu

---
+++ src/screens/AccountScreen/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
+import menus from './constant';
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+import colors from '../../constants/colors';
+
+import AccountsStyles from './style';
+
+export default function AccountScreen() {
+  return (
+    <View style={AccountsStyles.container}>
+      {menus.map((menu, index) => (
+        <TouchableOpacity key={index} style={AccountsStyles.listContainer}>
+          <View style={AccountsStyles.list}>
+            <MaterialIcons
+              name={menu.material_icon_name}
+              color={colors.primary}
+              size={30}
+              style={{marginRight: 15}}
+            />
+
+            <View>
+              <Text style={AccountsStyles.menuName}>{menu.name}</Text>
+              {menu.description !== '' && (
+                <Text style={AccountsStyles.menuDescriptionn}>
+                  {menu.description}
+                </Text>
+              )}
+            </View>
+          </View>
+
+          <MaterialIcons
+            name="arrow-forward-ios"
+            color={colors.primary}
+            size={30}
+          />
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+}
+const styles = StyleSheet.create({});
---
+++ src/screens/AccountScreen/style.js
@@ -0,0 +1,41 @@
+import {StyleSheet} from 'react-native';
+
+const AccountsStyles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    flex: 1,
+  },
+  textInputContainer: {
+    paddingVertical: 15,
+    flex: 1,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    marginRight: 10,
+  },
+  menuName: {
+    fontWeight: '600',
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  menuDescriptionn: {
+    fontSize: 14,
+  },
+  listContainer: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 15,
+    borderTopColor: '#D1D1D1',
+    borderTopWidth: 1,
+    flexDirection: 'row',
+  },
+  list: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+});
+
+export default AccountsStyles;
