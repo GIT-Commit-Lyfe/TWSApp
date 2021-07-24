4dec314c811163ba492af89df00939f2f998676c
lutfi <mlutfiibra@outlook.com>
2021-07-24 11:37:29 +0700

    Rata 458 (#6)
    * images for payment page
    
    * new template payment
    
    * update label
    
    * remove name label

--- src/screens/BoutiquesLocationScreen/index.js
+++ src/screens/BoutiquesLocationScreen/index.js
@@ -8,7 +8,8 @@ import {
   View,
 } from 'react-native';
 
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView from 'react-native-maps';
+import SwipeUpDown from 'react-native-swipe-up-down';
 
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
@@ -50,7 +51,6 @@ const BoutiquesLocationScreen = ({navigation}) => {
 
       <MapView
         style={{height: 500, width: '100%'}}
-        provider={PROVIDER_GOOGLE}
         region={{
           latitude: 37.78825,
           longitude: -122.4324,
@@ -60,51 +60,57 @@ const BoutiquesLocationScreen = ({navigation}) => {
         showsUserLocation={true}
       />
 
-      <FlatList
-        data={boutiques}
-        keyExtractor={(item, index) => index}
-        renderItem={({item, index}) => {
-          return (
-            <TouchableOpacity
-              style={BoutiquesLocationStyles.list}
-              onPress={() =>
-                navigation.navigate('Boutique', {name: item.boutiqueName})
-              }>
-              <Image
-                style={BoutiquesLocationStyles.image}
-                source={{uri: item.avatar}}
-              />
+      <SwipeUpDown
+        swipeHeight={100}
+        itemMini={
+          <FlatList
+            data={boutiques}
+            keyExtractor={(item, index) => index}
+            renderItem={({item, index}) => {
+              return (
+                <TouchableOpacity
+                  style={BoutiquesLocationStyles.list}
+                  onPress={() =>
+                    navigation.navigate('Boutique', {name: item.boutiqueName})
+                  }>
+                  <Image
+                    style={BoutiquesLocationStyles.image}
+                    source={{uri: item.avatar}}
+                  />
 
-              <View style={BoutiquesLocationStyles.copywriting}>
-                <View>
-                  <Jost500 style={BoutiquesLocationStyles.boutiqueName}>
-                    {item.boutiqueName}
-                  </Jost500>
-                  <Jost400 style={BoutiquesLocationStyles.address}>
-                    {item.address}
-                  </Jost400>
-                </View>
+                  <View style={BoutiquesLocationStyles.copywriting}>
+                    <View>
+                      <Jost500 style={BoutiquesLocationStyles.boutiqueName}>
+                        {item.boutiqueName}
+                      </Jost500>
+                      <Jost400 style={BoutiquesLocationStyles.address}>
+                        {item.address}
+                      </Jost400>
+                    </View>
 
-                <View>
-                  <Jost500 style={BoutiquesLocationStyles.estimatedTime}>
-                    {item.approximateTimeInMin + ' min '}
-                    <Jost400 style={BoutiquesLocationStyles.distance}>
-                      {`(${item.distance}${item.unit})`}
-                    </Jost400>
-                  </Jost500>
-                </View>
-              </View>
-              <MaterialIcons
-                name="chevron-right"
-                size={40}
-                color={colors.primary}
-              />
-            </TouchableOpacity>
-          );
-        }}
-        ItemSeparatorComponent={() => <Line />}
-        ListHeaderComponent={() => <Line />}
-        ListFooterComponent={() => <Line />}
+                    <View>
+                      <Jost500 style={BoutiquesLocationStyles.estimatedTime}>
+                        {item.approximateTimeInMin + ' min '}
+                        <Jost400 style={BoutiquesLocationStyles.distance}>
+                          {`(${item.distance}${item.unit})`}
+                        </Jost400>
+                      </Jost500>
+                    </View>
+                  </View>
+                  <MaterialIcons
+                    name="chevron-right"
+                    size={40}
+                    color={colors.primary}
+                  />
+                </TouchableOpacity>
+              );
+            }}
+            ItemSeparatorComponent={() => <Line />}
+            ListHeaderComponent={() => <Line />}
+            ListFooterComponent={() => <Line />}
+          />
+        }
+        disablePressToShow={false} // Press item mini to show full
       />
     </SafeAreaView>
   );
